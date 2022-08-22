f8dbfc03adb2c8f5d8183c5d5b330bbb68280e72
Brendon <brendon.lopes.21@gmail.com>
2022-08-22 19:19:39 -0300

    feat(users): :sparkles: adds validation to user's creation DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
+import { MessagesHelper } from 'src/helpers/messages.helper';
+import { RegExHelper } from 'src/helpers/regex.helper';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
+  @Matches(RegExHelper.password, { message: MessagesHelper.INVALID_PASSWORD })
   password: string;
 }
